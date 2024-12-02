ef951acff45f81f3ab09af7e1c49f68f7aa3aa02
Pratik <pratiksardar@gmail.com>
2024-12-02 11:35:39 +0530

    remove linting

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,21 +1,11 @@
 const path = require("path");
 
-const buildNextEslintCommand = (filenames) =>
-  `yarn next:lint --fix --file ${filenames
-    .map((f) => path.relative(path.join("packages", "nextjs"), f))
-    .join(" --file ")}`;
-
-const checkTypesNextCommand = () => "yarn next:check-types";
-
-const buildHardhatEslintCommand = (filenames) =>
-  `yarn hardhat:lint-staged --fix ${filenames
-    .map((f) => path.relative(path.join("packages", "hardhat"), f))
-    .join(" ")}`;
+// Disable linting by returning empty commands
+const buildNextEslintCommand = (filenames) => [];
+const checkTypesNextCommand = () => [];
+const buildHardhatEslintCommand = (filenames) => [];
 
 module.exports = {
-  "packages/nextjs/**/*.{ts,tsx}": [
-    buildNextEslintCommand,
-    checkTypesNextCommand,
-  ],
-  "packages/hardhat/**/*.{ts,tsx}": [buildHardhatEslintCommand],
+  "packages/nextjs/**/*.{ts,tsx}": [],
+  "packages/hardhat/**/*.{ts,tsx}": [],
 };
